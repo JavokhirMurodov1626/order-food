dfceb060d4c9711eaa2e335b933f42a855ee4ea8
Javokhir <javohir.murodov.9238@gmail.com>
2023-09-06 10:09:53 +0500

    adding header cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,14 @@
+import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import heroBanner from "../../assets/images/meals.jpg";
 function Header() {
   return (
     <>
-      <header className={classes.header}>
+      <div className={classes.header}>
         <h1>Vue Meals</h1>
-        <button>Cart</button>
-      </header>
+        <HeaderCartButton/>
+      </div>
+
       <div className={classes["main-image"]}>
         <img src={heroBanner} alt="A table that is full of delicious food" />
       </div>
